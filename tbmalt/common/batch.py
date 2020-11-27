b9cb164cfd2a9d73e17bd600d40923406945a9da
AJMHPC <adam.mcsloy@warwick.ac.uk>
2020-11-27 12:23:49 +0100

    Updated tbmalt.common.batch.pack function

--- tbmalt/common/batch.py
+++ tbmalt/common/batch.py
@@ -3,13 +3,16 @@
 This module contains classes and helper functions associated with batch
 construction, handling and maintenance.
 """
-from typing import Optional, Any, Tuple, List
+from typing import Optional, Any, Tuple, List, Union
+import numpy as np
 import torch
 Tensor = torch.Tensor
 
 
-def pack(tensors: List[Tensor], axis: int = 0, value: Any = 0,
-         size: Optional[Tuple[int]] = None) -> Tensor:
+def pack(tensors: List[Tensor],
+         axis: int = 0, value: Any = 0,
+         size: Optional[Union[Tuple[int], torch.Size]] = None
+         ) -> Tensor:
     """Pad and pack a sequence of tensors together.
 
     Pad a list of variable length tensors with zeros, or some other value, and
@@ -17,24 +20,23 @@ def pack(tensors: List[Tensor], axis: int = 0, value: Any = 0,
 
     Arguments:
         tensors: List of tensors to be packed, all with identical dtypes.
-        axis: Axis along which tensors will be packed; 0 for first axis
-            -1 for last axis, etc. This will be a new dimension. [DEFAULT=0]
+        axis: Axis along which tensors should be packed; 0 for first axis -1
+            for the last axis, etc. This will be a new dimension. [DEFAULT=0]
         value: The value with which the tensor is to be padded. [DEFAULT=0]
-        size: Specifies the size to which tensors should be padded. By default
-            tensors are padded to the size of the largest tensor. However,
-            ``max_size`` can be used to overwrite this behaviour.
+        size: Size of each dimension to which tensors should be padded. This
+            This to the largest size encountered along each dimension.
 
     Returns:
         packed_tensors: Input tensors padded and packed into a single tensor.
 
     Notes:
         ``packed_tensors`` maintains the same order as ``tensors``. This
-        is faster & more flexible than the internal pytorch p6ck & pad
+        is faster & more flexible than the internal pytorch pack & pad
         functions (at this particularly task).
 
     Examples:
 
-        >>> from dftbmalt.utils.batch import pack
+        >>> from tbmalt.common.batch import pack
         >>> import torch
         >>> a, b, c = torch.rand(2,2), torch.rand(3,3), torch.rand(4,4)
         >>> abc_packed_a = pack([a, b, c])
@@ -47,34 +49,27 @@ def pack(tensors: List[Tensor], axis: int = 0, value: Any = 0,
         >>> print(abc_packed_c.shape)
         torch.Size([4, 4, 3])
 
-    Todo:
-        - Expand functionality to allow packing of systems of arbitrary shape
-            and dimensionality.
     """
+    # Gather some general setup info
+    count, device, dtype = len(tensors), tensors[0].device, tensors[0].dtype
 
-    # If "size" unspecified; the maximum observed size along each axis is used
-    if size is None:
-        size = torch.max(torch.tensor([i.shape for i in tensors]), 0)[0]
+    # Identify the maximum size, if one was not specified.
+    size = np.max([i.shape for i in tensors], 0) if size is None else size
 
-    # Create a tensor to pack into & fill with padding value. Work under the
-    # assumption that "axis" == 0 and permute later on (easier this way).
-    padded = torch.empty(len(tensors), *size,
-                         dtype=tensors[0].dtype,
-                         device=tensors[0].device).fill_(value)
+    # Tensor to pack into, filled with padding value.
+    padded = torch.full((count, *size), value, dtype=dtype, device=device)
 
-    # Loop over tensors & the dimension of "padded" it is to be packed into.
-    # This loop is not an elegant solution, but it is fast.
-    for n, t in enumerate(tensors):
-        # Pack via slice operations to allow code to be dimension agnostic.
-        slices = (slice(0, s) for s in t.shape)
-        padded[(n, *slices)] = t
+    # Loop over & pack "tensors" into "padded". A proxy index "n" must be used
+    # for assignments rather than a slice to prevent in-place errors.
+    for n, source in enumerate(tensors):
+        # Slice operations not elegant but they are dimension agnostic & fast.
+        padded[(n, *[slice(0, s) for s in source.shape])] = source
 
-    # If "axis" was anything other than 0, then padded must be permuted
+    # If "axis" was anything other than 0, then "padded" must be permuted.
     if axis != 0:
-        # Resolve negative "axis" values to their 'positive' equivalents to
-        # maintain expected slicing behaviour when using the insert function.
-        if axis < 0:
-            axis = padded.dim() + 1 + axis
+        # Resolve relative any axes to their absolute equivalents to maintain
+        # expected slicing behaviour when using the insert function.
+        axis = padded.dim() + 1 + axis if axis < 0 else axis
 
         # Build a list of axes indices; but omit the axis on which the data
         # was concatenated (i.e. 0).
@@ -86,9 +81,4 @@ def pack(tensors: List[Tensor], axis: int = 0, value: Any = 0,
         # Perform the permeation
         padded = padded.permute(ax)
 
-    # Return the packed tensor
     return padded
-
-
-def unpack(tensor: Tensor) -> List[Tensor]:
-    pass
