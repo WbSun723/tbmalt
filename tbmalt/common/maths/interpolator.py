78cfa24ce74e56affc7fb6de744d76a03dec2e71
FanGuozheng <gfan@uni-bremen.de>
2020-12-16 15:10:59 +0100

    documentation 	modified:   tbmalt/common/maths/interpolator.py

--- tbmalt/common/maths/interpolator.py
+++ tbmalt/common/maths/interpolator.py
@@ -1,7 +1,5 @@
 import bisect
 import torch
-from typing import Tuple, Union, Optional
-from numbers import Real
 Tensor = torch.Tensor
 
 
@@ -21,7 +19,7 @@ class BicubInterp:
         zmesh: a 4D or 5D Tensor
 
     Reference:
-        .. https://en.wikipedia.org/wiki/Bicubic_interpolation
+        .. [Bicubic wiki] https://en.wikipedia.org/wiki/Bicubic_interpolation
     """
 
     def __init__(self, xmesh: Tensor, zmesh: Tensor):
@@ -29,34 +27,21 @@ class BicubInterp:
         self.xmesh = xmesh
         self.zmesh = zmesh
 
-    def __call__(self, xi: Tensor):
+    def __call__(self, xnew: Tensor):
         """Calculate bicubic interpolation.
 
         Arguments:
-            xi: the interpolation points of each atom in molecule
-
-        Notes:
-            For each bicubic interpolation, there is 4 surrounding grid points
-            and 12 second nearest neighbors. We use _ to represent previous
-            points.
+            xnew: the interpolation points of each atom in molecule
         """
-        self.xi = xi
-        xi_ = self.xi.cpu() if self.xi.device.type == 'cuda' else self.xi
+        self.xi = xnew
+        self.nat = len(self.xi)
         coeff = torch.tensor([[1., 0., 0., 0.], [0., 0., 1., 0.],
                               [-3., 3., -2., -1.], [2., -2., 1., 1.]])
         coeff_ = torch.tensor([[1., 0., -3., 2.], [0., 0., 3., -2.],
                                [0., 1., -2., 1.], [0., 0., -1., 1.]])
 
         # get the nearest grid points indices of xi
-        self.nx0 = [bisect.bisect(self.xmesh[ii].cpu().detach().numpy(),
-                                  xi_[ii].cpu().detach().numpy()) - 1
-                    for ii in range(len(self.xi))]
-
-        # get all surrounding 4 grid points indices
-        self.nind = [ii for ii in range(len(self.xi))]
-        self.nx1 = [ii + 1 for ii in self.nx0]
-        self.nx_1 = [ii - 1 if ii >= 1 else ii for ii in self.nx0]
-        self.nx2 = [ii + 2 if ii <= len(self.xmesh) - 3 else ii + 1 for ii in self.nx0]
+        self._get_indices()
 
         # this is to transfer x to fraction
         xf = (self.xi - self.xmesh.T[self.nx0, self.nind]) / (self.xmesh.T[
@@ -64,11 +49,11 @@ class BicubInterp:
         xmat = torch.stack([xf ** 0, xf ** 1, xf ** 2, xf ** 3])
 
         # get four nearest grid points values, each will be: [natom, natom, 20]
-        f00, f10, f01, f11 = self.fmat0th(self.zmesh)
+        f00, f10, f01, f11 = self._fmat0th(self.zmesh)
 
         # get four nearest grid points derivative over x, y, xy
         f02, f03, f12, f13, f20, f21, f30, f31, f22, f23, f32, f33 = \
-            self.fmat1th(self.zmesh, f00, f10, f01, f11)
+            self._fmat1th(self.zmesh, f00, f10, f01, f11)
         fmat = torch.stack([torch.stack([f00, f01, f02, f03]),
                             torch.stack([f10, f11, f12, f13]),
                             torch.stack([f20, f21, f22, f23]),
@@ -78,66 +63,81 @@ class BicubInterp:
         a_mat = torch.matmul(torch.matmul(coeff, fmat.permute(pdim)), coeff_)
         return torch.stack([torch.stack(
             [torch.matmul(torch.matmul(xmat[:, i], a_mat[i, j]), xmat[:, j])
-             for j in range(len(xi))]) for i in range(len(xi))])
+             for j in range(self.nat)]) for i in range(self.nat)])
 
-    def fmat0th(self, zmesh):
+    def _get_indices(self):
+        """Get indices and repeat indices."""
+        xi = self.xi.cpu() if self.xi.device.type == 'cuda' else self.xi
+        xm = self.xmesh.cpu() if self.xmesh.device.type == 'cuda' else self.xmesh
+        self.nx0 = torch.tensor([
+            bisect.bisect(xm[ii].detach().numpy(), xi[ii].detach().numpy()) - 1
+            for ii in range(self.nat)])
+        self.nx0ri = self.nx0.repeat_interleave(self.nat)
+        self.nx0r = self.nx0.repeat(self.nat)
+
+        # get all surrounding 4 grid points indices and repeat indices
+        self.nind = torch.tensor([ii for ii in range(self.nat)])
+        self.nindri = self.nind.repeat_interleave(self.nat)
+        self.nindr = self.nind.repeat(self.nat)
+        self.nx1 = torch.tensor([ii + 1 for ii in self.nx0])
+        self.nx1ri = self.nx1.repeat_interleave(self.nat)
+        self.nx1r = self.nx1.repeat(self.nat)
+        self.nx_1 = torch.tensor([ii - 1 if ii >= 1 else ii for ii in self.nx0])
+        self.nx_1ri = self.nx_1.repeat_interleave(self.nat)
+        self.nx_1r = self.nx_1.repeat(self.nat)
+        self.nx2 = torch.tensor([ii + 2 if ii <= len(self.xmesh) - 3
+                                 else ii + 1 for ii in self.nx0])
+        self.nx2ri = self.nx2.repeat_interleave(self.nat)
+        self.nx2r = self.nx2.repeat(self.nat)
+
+    def _fmat0th(self, zmesh):
         """Construct f(0/1, 0/1) in fmat."""
-        f00 = torch.stack([torch.stack([zmesh[i, j, self.nx0[i], self.nx0[j]]
-                                        for j in self.nind]) for i in self.nind])
-        f10 = torch.stack([torch.stack([zmesh[i, j, self.nx1[i], self.nx0[j]]
-                                        for j in self.nind]) for i in self.nind])
-        f01 = torch.stack([torch.stack([zmesh[i, j, self.nx0[i], self.nx1[j]]
-                                        for j in self.nind]) for i in self.nind])
-        f11 = torch.stack([torch.stack([zmesh[i, j, self.nx1[i], self.nx1[j]]
-                                        for j in self.nind]) for i in self.nind])
+        f00 = zmesh[self.nindri, self.nindr, self.nx0ri, self.nx0r].reshape(
+            self.nat, self.nat, -1).squeeze(-1)
+        f10 = zmesh[self.nindri, self.nindr, self.nx1ri, self.nx0r].reshape(
+            self.nat, self.nat, -1).squeeze(-1)
+        f01 = zmesh[self.nindri, self.nindr, self.nx0ri, self.nx1r].reshape(
+            self.nat, self.nat, -1).squeeze(-1)
+        f11 = zmesh[self.nindri, self.nindr, self.nx1ri, self.nx1r].reshape(
+            self.nat, self.nat, -1).squeeze(-1)
         return f00, f10, f01, f11
 
-    def fmat1th(self, zmesh, f00, f10, f01, f11):
+    def _fmat1th(self, zmesh, f00, f10, f01, f11):
         """Get the 1st derivative of four grid points over x, y and xy."""
-        f_10 = torch.stack([torch.stack([zmesh[i, j, self.nx_1[i], self.nx0[j]]
-                                         for j in self.nind]) for i in self.nind])
-        f_11 = torch.stack([torch.stack([zmesh[i, j, self.nx_1[i], self.nx1[j]]
-                                         for j in self.nind]) for i in self.nind])
-        f0_1 = torch.stack([torch.stack([zmesh[i, j, self.nx0[i], self.nx_1[j]]
-                                         for j in self.nind]) for i in self.nind])
-        f02 = torch.stack([torch.stack([zmesh[i, j, self.nx0[i], self.nx2[j]]
-                                        for j in self.nind]) for i in self.nind])
-        f1_1 = torch.stack([torch.stack([zmesh[i, j, self.nx1[i], self.nx_1[j]]
-                                         for j in self.nind]) for i in self.nind])
-        f12 = torch.stack([torch.stack([zmesh[i, j, self.nx1[i], self.nx2[j]]
-                                        for j in self.nind]) for i in self.nind])
-        f20 = torch.stack([torch.stack([zmesh[i, j, self.nx2[i], self.nx0[j]]
-                                        for j in self.nind]) for i in self.nind])
-        f21 = torch.stack([torch.stack([zmesh[i, j, self.nx2[i], self.nx1[j]]
-                                        for j in self.nind]) for i in self.nind])
+        f_10 = zmesh[self.nindri, self.nindr, self.nx_1ri, self.nx0r].reshape(
+            self.nat, self.nat, -1).squeeze(-1)
+        f_11 = zmesh[self.nindri, self.nindr, self.nx_1ri, self.nx_1r].reshape(
+            self.nat, self.nat, -1).squeeze(-1)
+        f0_1 = zmesh[self.nindri, self.nindr, self.nx0ri, self.nx_1r].reshape(
+            self.nat, self.nat, -1).squeeze(-1)
+        f02 = zmesh[self.nindri, self.nindr, self.nx0ri, self.nx2r].reshape(
+            self.nat, self.nat, -1).squeeze(-1)
+        f1_1 = zmesh[self.nindri, self.nindr, self.nx_1ri, self.nx_1r].reshape(
+            self.nat, self.nat, -1).squeeze(-1)
+        f12 = zmesh[self.nindri, self.nindr, self.nx1ri, self.nx2r].reshape(
+            self.nat, self.nat, -1).squeeze(-1)
+        f20 = zmesh[self.nindri, self.nindr, self.nx2ri, self.nx0r].reshape(
+            self.nat, self.nat, -1).squeeze(-1)
+        f21 = zmesh[self.nindri, self.nindr, self.nx2ri, self.nx1r].reshape(
+            self.nat, self.nat, -1).squeeze(-1)
 
         # calculate the derivative: (F(1) - F(-1) / (2 * grid)
-        # if there is no previous or next grdi point, it will be:
-        # (F(1) - F(0) / grid or (F(0) - F(-1) / grid
-        fy00 = torch.stack([torch.stack([(f01[i, j] - f0_1[i, j]) /
-                                         (self.nx1[j] - self.nx_1[j])
-                                         for j in self.nind]) for i in self.nind])
-        fy01 = torch.stack([torch.stack([(f02[i, j] - f00[i, j]) /
-                                         (self.nx1[j] - self.nx_1[j])
-                                         for j in self.nind]) for i in self.nind])
-        fy10 = torch.stack([torch.stack([(f11[i, j] - f1_1[i, j]) /
-                                         (self.nx1[j] - self.nx_1[j])
-                                         for j in self.nind]) for i in self.nind])
-        fy11 = torch.stack([torch.stack([(f12[i, j] - f10[i, j]) /
-                                         (self.nx1[j] - self.nx_1[j])
-                                         for j in self.nind]) for i in self.nind])
-        fx00 = torch.stack([torch.stack([(f10[i, j] - f_10[i, j]) /
-                                         (self.nx1[i] - self.nx_1[i])
-                                         for j in self.nind]) for i in self.nind])
-        fx01 = torch.stack([torch.stack([(f20[i, j] - f00[i, j]) /
-                                         (self.nx1[i] - self.nx_1[i])
-                                         for j in self.nind]) for i in self.nind])
-        fx10 = torch.stack([torch.stack([(f11[i, j] - f_11[i, j]) /
-                                         (self.nx1[i] - self.nx_1[i])
-                                         for j in self.nind]) for i in self.nind])
-        fx11 = torch.stack([torch.stack([(f21[i, j] - f01[i, j]) /
-                                         (self.nx1[i] - self.nx_1[i])
-                                         for j in self.nind]) for i in self.nind])
+        fy00 = (f01 - f0_1)[self.nindri, self.nindr].reshape(
+            self.nat, self.nat, -1).squeeze(-1) / (self.nx1 - self.nx_1)
+        fy01 = (f02 - f00)[self.nindri, self.nindr].reshape(
+            self.nat, self.nat, -1).squeeze(-1) / (self.nx1 - self.nx_1)
+        fy10 = (f11 - f1_1)[self.nindri, self.nindr].reshape(
+            self.nat, self.nat, -1).squeeze(-1) / (self.nx1 - self.nx_1)
+        fy11 = (f12 - f10)[self.nindri, self.nindr].reshape(
+            self.nat, self.nat, -1).squeeze(-1) / (self.nx1 - self.nx_1)
+        fx00 = (f10 - f_10)[self.nindri, self.nindr].reshape(
+            self.nat, self.nat, -1).squeeze(-1) / (self.nx1 - self.nx_1)
+        fx01 = (f20 - f00)[self.nindri, self.nindr].reshape(
+            self.nat, self.nat, -1).squeeze(-1) / (self.nx1 - self.nx_1)
+        fx10 = (f11 - f_11)[self.nindri, self.nindr].reshape(
+            self.nat, self.nat, -1).squeeze(-1) / (self.nx1 - self.nx_1)
+        fx11 = (f21 - f01)[self.nindri, self.nindr].reshape(
+            self.nat, self.nat, -1).squeeze(-1) / (self.nx1 - self.nx_1)
         fxy00, fxy11 = fy00 * fx00, fx11 * fy11
         fxy01, fxy10 = fx01 * fy01, fx10 * fy10
         return fy00, fy01, fy10, fy11, fx00, fx01, fx10, fx11, fxy00, fxy01, \
@@ -153,10 +153,7 @@ class Spline1d:
 
     Keyword Args:
         kind: string, define spline method, default is cubic
-        abcd: a tuple of Tensor
-
-    Methods:
-        __call__
+        abcd: a tuple of Tensor for cubic spline
 
     Examples:
         >>> import tbmalt.common.maths.interpolator as interp
@@ -187,7 +184,7 @@ class Spline1d:
             if kwargs.get('abcd') is not None:
                 self.aa, self.bb, self.cc, self.dd = kwargs.get('abcd')
             else:
-                self.aa, self.bb, self.cc, self.dd = self.get_abcd()
+                self.aa, self.bb, self.cc, self.dd = self._get_abcd()
         elif kind == 'linear':
             self.kind = 'linear'
         else:
@@ -218,24 +215,24 @@ class Spline1d:
         self.ind = [torch.arange(len(self.xnew)), self.dind] if self.batch else self.dind
 
         if self.kind == 'cubic':
-            return self.cubic()
+            return self._cubic()
         elif self.kind == 'linear':
-            return self.linear()
+            return self._linear()
 
-    def linear(self):
+    def _linear(self):
         """Calculate linear interpolation."""
         return self.yp[self.ind] + (self.xnew - self.xp[self.dind]) / (
             self.xp[1:] - self.xp[:-1])[self.dind] * (
             self.yp[..., 1:] - self.yp[..., :-1])[self.ind]
 
-    def cubic(self):
+    def _cubic(self):
         """Calculate cubic spline interpolation."""
         # calculate a, b, c, d parameters, need input x and y
         dx = self.xnew - self.xp[self.dind]
         return self.aa[self.ind] + self.bb[self.ind] * dx + \
             self.cc[self.ind] * dx ** 2 + self.dd[self.ind] * dx ** 3
 
-    def get_abcd(self):
+    def _get_abcd(self):
         """Get parameter aa, bb, cc, dd for cubic spline interpolation."""
         # get the first dim of x
         nx = self.xp.shape[0]
