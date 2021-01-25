46ca18b12ac0009d1e0ecd448f70ff02efb17926
FanGuozheng <gfan@uni-bremen.de>
2021-01-25 20:58:55 +0100

    add periodic

---
+++ tbmalt/common/structures/periodic.py
@@ -0,0 +1,12 @@
+"""Deal with periodic conditions."""
+import torch
+Tensor = torch.Tensor
+
+
+def get_cell_translations():
+    """"""
+    pass
+
+def get_neighbour(distance: Tensor, cutoff):
+    """"""
+    pass
